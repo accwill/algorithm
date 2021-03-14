e83cd28d5e195e6b037bcc21f95841ef12681ffc
宋亚奇 <193324007@qq.com>
2021-03-14 09:16:57 +0800

    update 添加递归阶乘

---
+++ recursion/fibonacciIterative.js
@@ -0,0 +1,18 @@
+function fibonacciIterative(n) {
+  if (n < 1) return 0;
+  if (n <= 2) return 1;
+
+  let fibNMinus2 = 0;
+  let fibNMinus1 = 1;
+  let fibN = n;
+  for (let i = 2; i <= n; i++) { // n >= 2
+    fibN = fibNMinus1 + fibNMinus2; // f(n-1) + f(n-2)
+    fibNMinus2 = fibNMinus1;
+    fibNMinus1 = fibN;
+    console.log('dkj', fibN)
+  }
+  return fibN;
+}
+
+
+console.log('fdlsfj', fibonacciIterative(8))
